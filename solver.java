7ba6e49d60138061f87b5d177369e0388f429c2c
saifrahmn <saifrahmn78@gmail.com>
2022-06-19 13:12:18 +0530

    writing the solvers code

---
+++ solver.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class solver{
+    public static void main(String[] args){
+        int[][] a= new int[9][9];
+        Scanner sc=new Scanner(System.in);
+        for (int i=0;i<=8;i++){
+            for(int j=0;j<=8;j++){
+                System.out.printf("Enter the value of a "+ i +j);
+                a[i][j]= sc.nextInt();
+            }
+        }
+        for (int i=0;i<=8;i++){
+            for(int j=0;j<=8;j++){
+                System.out.print(a[i][j]);
+                if(j==2||j==5||j==8){
+                    System.out.print("  ");
+                }
+            }
+            System.out.println("");
+            
+        }
+    }
+}
