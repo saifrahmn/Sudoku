93770e8b54eea48d526a52dd8a4fa7c263fb52e1
saifrahmn <saifrahmn78@gmail.com>
2022-09-11 00:34:10 +0530

    update

---
+++ script/app.js
@@ -0,0 +1,28 @@
+var grid= new Array(9);
+for(let i=0;i<=8;i++){
+	grid[i]= new Array(9);
+}
+
+
+for(let i=0;i<=8;i++){
+    if(grid[i][0]!=null){
+        var v=grid[i][0];
+        if(i%3>=2){
+            for(let j=0;j<=8;j++){
+                if(j==3||j==4||j==5){
+                    continue;
+                }
+            }
+        }
+        else if(2>i%3>=1){
+            for(let j=3;j<=5;j++){
+
+            }
+        }
+        else{
+            for(let j=0;j<=2;j++){
+
+            }
+        }
+    }
+}
